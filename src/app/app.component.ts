5c6bbb3ac3846c0d167ac90e9d5cb4e6770cb3db
Denys Linux <denys.msi04@gmail.com>
2024-03-09 17:38:52 +0100

    ngModel and ngModelChange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/WishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,5 +16,27 @@ export class AppComponent {
     new WishItem(2, 'Get coffee', true),
     new WishItem(3, 'Find grass that cuts itself'),
   ];
+
   title = 'wishlist';
+  newWishText = '';
+  listFilter : string = '0';
+  visibleItems: WishItem[] = this.items;
+  
+  toggleItem(item : WishItem){ //Para ver todos los parametros del evento
+    item.isComplete = !item.isComplete;
+    alert(item);
+  }
+  addNewWish(){
+    this.items.push(new WishItem(3, this.newWishText));
+    this.newWishText='';
+  }
+  
+  filterChanged(value: any){ 
+    switch(value){
+      case '0': this.visibleItems = this.items; break;
+      case '1': this.visibleItems = this.items.filter(item => !item.isComplete); break;
+      case '2': this.visibleItems = this.items.filter(item => item.isComplete);  break;
+    }
+    alert(this.visibleItems);  
+  }
 }
