adda87505b6df01711d95a30485d9651e7709102
Denys Linux <denys.msi04@gmail.com>
2024-03-09 14:07:00 +0100

    decisions binding data arrays and data model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/WishItem';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items : WishItem[] = [
+    new WishItem(1, 'To learn angular'),
+    new WishItem(2, 'Get coffee', true),
+    new WishItem(3, 'Find grass that cuts itself'),
+  ];
   title = 'wishlist';
 }
---
+++ src/shared/models/WishItem.ts
@@ -0,0 +1,5 @@
+export class WishItem{
+  constructor(public wishId: Number, public wishText: string, public isComplete: boolean = false){
+    
+  }
+}
